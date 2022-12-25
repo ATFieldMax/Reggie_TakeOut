1fcb18e6e5ec9b2f61348da0c8920c821663c854
Bern <peter1999719@gmail.com>
2022-12-26 06:38:09 +0800

    設置Redis

---
+++ src/main/java/com/example/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+//設置序列化器
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object,Object> redisTemplate=new RedisTemplate<>();
+
+        //默認的Key序列化器為JdkSerializationRedisSerializer
+        //Value使用默認的序列化器即可，不需更改
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        return redisTemplate;
+    }
+}
